55897e68913e91d595cdc39d530a2b9e9f1513df
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-16 18:47:10 +0530

    new files

---
+++ AdvanceJAVA/ClassAndObjects.java
@@ -0,0 +1,9 @@
+package AdvanceJAVA;
+
+public class ClassAndObjects {
+    int data = 769;
+    public static void main(String... args) {
+        ClassAndObjects obj1 = new ClassAndObjects();
+        System.out.println(obj1.data);
+    }
+}
