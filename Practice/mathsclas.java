c51a40eadf5af3cf22df6d678b72192fac55b578
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-16 13:49:34 +0530

    new files added

---
+++ Practice/mathsclas.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class mathsclas {
+    public static void main(String... args) {
+        System.out.println(Math.max(5,9));
+        System.out.println(5>9);
+        
+        char[] a = {'a' , 'b' , 'c' , 'd'};
+        System.out.println(a);
+    }
+}
---
+++ Practice/methodOverloading.java
@@ -0,0 +1,31 @@
+package Practice;
+
+public class methodOverloading{
+    public static void main(String... args){
+        myMethod();
+        System.out.println(myMethod(5.66, 2));
+        System.out.println(myMethod(10, 98));
+        System.out.println(myMethod("Hello ", "Nikhil"));
+        System.out.println(myMethod("Growth Natives", 5));
+    }
+
+    static void myMethod(){
+        System.out.println("Normal method without parameter and return value is called.");
+    }
+
+    static int myMethod(int a , int b){
+        return a+b;
+    }
+    
+    static double myMethod(Double a , int b){
+        return a+b;
+    }
+    
+    static char myMethod(String a , int b){
+        return a.charAt(b);
+    }
+    
+    static String myMethod(String a , String b){
+        return a+b;
+    }
+}
