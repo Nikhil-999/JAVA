07945187c52af32cf7b8d9f3d2b1788ca5dfa582
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-16 12:08:28 +0530

    new folder practice is added

---
+++ Practice/palindrome.java
@@ -0,0 +1,20 @@
+public class palindrome {
+    public static void main(String... args) {
+        String a = "able was I ere I saw elba";
+        System.out.println(ispalindrome(a));
+    }
+
+    static boolean ispalindrome(String a){
+        int n = a.length();
+        int l=0 , r=n-1;
+        while(l<=r){
+            if(a.charAt(l) != a.charAt(r))
+            return false;
+            r--;
+            l++;
+        }
+
+
+        return true;
+    }
+}
---
+++ Practice/reverse.java
@@ -0,0 +1,13 @@
+public class reverse{
+    public static void main(String... args){
+        String demo = "Nikhil Singh";
+        String ans = "";
+        System.out.println(demo);
+        int n = demo.length();
+        for(int i=0 ; i<n ; i++){
+            char temp = demo.charAt(n-1-i);
+            ans += temp;
+        }
+        System.out.println(ans);
+    }
+}
