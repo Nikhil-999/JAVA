bee69598dce892d061d4596c3c45d8d18763be90
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-20 15:22:50 +0530

    #

--- AdvanceJAVA/Polymorphism.java
+++ AdvanceJAVA/Polymorphism.java
@@ -1,26 +1,32 @@
 package AdvanceJAVA;
 
-class human{
-    public void sound(){
+public class Polymorphism {
+    public static void main(String[] args) {
+        human Ohuman = new human();
+        parent Oparent = new parent();
+        child Ochild = new child();
+        human Ochild2 = new child();        //Creates a child object.
+        Ohuman.sound();
+        Oparent.sound();
+        Ochild.sound();
+        Ochild2.sound();
+    }
+}
+
+class human {
+    public void sound() {
         System.out.println("Human sound ...!!");
     }
 }
 
-class parent extends human{
-    public void sound(){
+class parent extends human {
+    public void sound() {
         System.out.println("This is the sound of a PARENT");
     }
 }
 
-class child extends human{
-    public void sound(){
+class child extends human {
+    public void sound() {
         System.out.println("This is the sound of a CHILD");
-    }
-}
-
-public class Polymorphism {
-    public static void main(String[] args) {
-        child Oc = new child();
-        Oc.sound();
     }
 }
