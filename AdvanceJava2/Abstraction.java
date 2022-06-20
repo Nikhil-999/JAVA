cdf816a99aad751c1f4819f20bbc9ead1a60822a
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-20 18:58:57 +0530

    New files about abstraction

---
+++ AdvanceJava2/Abstraction.java
@@ -0,0 +1,27 @@
+package AdvanceJava2;
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Child22 obj = new Child22();
+        obj.childFun();
+        obj.sound();
+    }
+}
+
+class Child22 extends parent{
+    public void childFun(){
+        System.out.println("this is child function inside child class.");
+    }
+    public void sound(){
+        System.out.println("This is sound function which is declared inside parent class but its body is given inside child class.");
+    }
+}
+
+abstract class parent{
+        public abstract void sound();
+        public void myFun(){
+            System.out.println("This is my function inside abstract class.");
+        }
+}
+
+
