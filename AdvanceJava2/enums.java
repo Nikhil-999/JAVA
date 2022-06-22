f7b3cfc10ca3aae48f7e15db397856a9ff656bb9
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-22 12:15:19 +0530

    #

--- AdvanceJava2/enums.java
+++ AdvanceJava2/enums.java
@@ -4,11 +4,26 @@ public class enums {
 
     enum demo{RED,ORANGE,GREEN,BLUE}
     public static void main(String[] args) {
-        demo a = demo.BLUE;
+        demo a = demo.ORANGE;
         System.out.println(a);
         demo arr[] = demo.values();
         for(demo x: arr){
             System.out.print(x + " ");
+        }
+        System.out.println();
+        switch(a){
+            case RED:
+            System.out.println("this is constant RED");
+            break;
+            case ORANGE:
+            System.out.println("this is constant ORANGE");
+            break;
+            case BLUE:
+            System.out.println("this is constant BLUE");
+            break;
+            case GREEN:
+            System.out.println("this is constant GREEN");
+            break;
         }
     }
 }
