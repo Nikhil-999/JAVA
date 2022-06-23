8e162cf1614b205365aacfe66e57f2e2ecbef38a
Nikhil Singh <iamnikhil2409@gmail.com>
2022-06-23 13:35:35 +0530

    new file about queue is added

---
+++ CollectionsInJava/PriorityQueueInJava.java
@@ -0,0 +1,35 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueInJava {
+    public static void main(String[] args) {
+        //  PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //  or
+        Queue<Integer> pq = new PriorityQueue<>();      //this is by default min heap implementation.
+        Queue<Integer> pq2 = new PriorityQueue<>(Comparator.reverseOrder());        //this is max-heap implementation.
+
+        pq.offer(111);
+        pq.offer(888);
+        pq.offer(222);
+        pq.offer(6660);
+
+        
+        pq2.offer(111);
+        pq2.offer(888);
+        pq2.offer(222);
+        pq2.offer(6660);
+
+        System.out.println(pq);     //Give priority to head element and suffle the queue such that head contain the smallest element in the queue.
+        System.out.println(pq2);    //Give priority to head element and suffle the queue such that head contain largest element in the queue.
+
+        System.out.println(pq.poll());
+        System.out.println(pq2.poll());
+
+        System.out.println(pq);
+        System.out.println(pq2);
+
+        System.out.println(pq.peek());
+        System.out.println(pq2.peek());
+    }
+}
---
+++ CollectionsInJava/QueueInJava.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class QueueInJava {
+    public static void main(String[] args) {
+        Queue<Integer> demo = new LinkedList<>();
+        demo.offer(565);
+        demo.offer(111);
+        demo.offer(7863);
+        System.out.println(demo);
+        System.out.println(demo.poll());
+        System.out.println(demo);
+        System.out.println(demo.peek());
+
+        demo.add(4444);
+        demo.add(5555);
+        demo.add(6666);
+        demo.add(7777);
+        
+    }    
+}
