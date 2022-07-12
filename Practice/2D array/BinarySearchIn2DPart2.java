014321d7446a2fd5a856eeccffa54f4b62438fd5
Nikhil Singh <iamnikhil2409@gmail.com>
2022-07-12 18:50:25 +0530

    updated binary search part 2 solution better approach

--- Practice/2D array/BinarySearchIn2DPart2.java
+++ Practice/2D array/BinarySearchIn2DPart2.java
@@ -5,31 +5,52 @@ public class BinarySearchIn2DPart2 {
         System.out.println("\n\n"+searchMatrix(matrix,5)+"\n");
     }
 
+    // public static boolean searchMatrix(int[][] matrix, int target) {
+    //     int start = 0;
+    //     int end = matrix.length - 1;
+
+    //     while (start <= end) {
+    //         int INstart = 0;
+    //         int INend = matrix[start].length - 1;
+    //         int INmid = INstart + (INend - INstart) / 2;
+
+    //         while (INstart <= INend) {
+    //             if (matrix[start][INmid] == target)
+    //                 return true;
+
+    //             if (matrix[start][INmid] < target) {
+    //                 INstart = INmid + 1;
+    //             } else {
+    //                 INend = INmid - 1;
+    //             }
+    //             INmid = INstart + (INend - INstart) / 2;
+    //         }
+
+    //         start++;
+
+    //     }
+
+    //     return false;
+    // }
+
     public static boolean searchMatrix(int[][] matrix, int target) {
-        int start = 0;
-        int end = matrix.length - 1;
+        int row = matrix.length;
 
-        while (start <= end) {
-            int INstart = 0;
-            int INend = matrix[start].length - 1;
-            int INmid = INstart + (INend - INstart) / 2;
+        int indexRow = 0 , indexCol = matrix[0].length - 1;
+        while(indexRow < row && indexCol >= 0){
+            if(matrix[indexRow][indexCol] == target)
+            return true;
 
-            while (INstart <= INend) {
-                if (matrix[start][INmid] == target)
-                    return true;
-
-                if (matrix[start][INmid] < target) {
-                    INstart = INmid + 1;
-                } else {
-                    INend = INmid - 1;
-                }
-                INmid = INstart + (INend - INstart) / 2;
+            if(matrix[indexRow][indexCol] > target){
+                indexCol--;
             }
-
-            start++;
-
+            else{
+                indexRow++;
+            }
         }
 
         return false;
     }
+
+
 }
