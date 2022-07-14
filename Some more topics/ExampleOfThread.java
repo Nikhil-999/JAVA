62d1acd17ce355ec1c8cd4d3a35336a0f93a545d
Nikhil Singh <iamnikhil2409@gmail.com>
2022-07-14 12:56:46 +0530

    changes

--- Some more topics/ExampleOfThread.java
+++ Some more topics/ExampleOfThread.java
@@ -1,9 +1,14 @@
 public class ExampleOfThread extends Thread{
-    public void Hello(){
+    public void run(){
         System.out.println("Hello function is running.");
     }
     public static void main(String[] args) {
         ExampleOfThread obj = new ExampleOfThread();
         obj.start();
+
+
+        //here run function is already defined in thread class but 
+        // as we have now implemented a function with the same name
+        // so now it will be printed.
     }
 }
